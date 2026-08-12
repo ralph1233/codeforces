cf46f328cecaf0a646490c4fe5a922de52938d6f
ralph1233 <ralphmerhy93@gmail.com>
2026-08-12 13:14:31 +0300

    .

---
+++ 800/Maximum GCD.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  int t;
+  std::vector<int> res;
+
+  std::cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    int nb;
+
+    std::cin >> nb;
+
+    res.push_back(nb / 2);
+  }
+
+  for (int i = 0; i < t; i++) {
+    std::cout << res[i] << "\n";
+  }
+
+  return 0;
+}
